0b9310639d36c2de38352a769cf90e9ab8bb5273
lehoangson1311 <lehoangson1311@gmail.com>
2023-05-31 20:47:14 +0700

    session-02-vi du thay doi state 2

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import "./App.css";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { studentName: "Trung" };
-    this.changeStudentName = this.changeStudentName.bind(this);
+    this.state = { number: 0 };
   }
-  changeStudentName() {
-    this.setState({ studentName: "Sáng" });
+  increaseNumber() {
+    this.setState({
+      number: this.state.number + 1,
+    });
   }
   render() {
     return (
       <div>
-        student name:{this.state.studentName}
-        <button onClick={this.changeStudentName}>change name</button>
+        <p>Number:{this.state.number}</p>
+        <button onClick={() => this.increaseNumber()}>Tăng lên 1</button>
       </div>
     );
   }
