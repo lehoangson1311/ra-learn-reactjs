b9408e80a525c999d6bc3aae6e2655bb4e79d94f
lehoangson1311 <lehoangson1311@gmail.com>
2023-05-31 20:22:36 +0700

    session-02-vi du thay doi state

--- src/App.js
+++ src/App.js
@@ -6,10 +6,18 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = { studentName: "Trung" };
+    this.changeStudentName = this.changeStudentName.bind(this);
   }
-
+  changeStudentName() {
+    this.setState({ studentName: "Sáng" });
+  }
   render() {
-    return <div>student name:{this.state.studentName}</div>;
+    return (
+      <div>
+        student name:{this.state.studentName}
+        <button onClick={this.changeStudentName}>change name</button>
+      </div>
+    );
   }
 }
 
