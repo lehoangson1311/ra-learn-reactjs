bc9872aa0bba3eef9df34a43ed6c79c73ebbd6e3
lehoangson1311 <lehoangson1311@gmail.com>
2023-06-05 20:33:42 +0700

    session4-vi du useState2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  // useEffect(function () {
+  //   document.title = `You clicked ${count} times`;
+  // });
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> You clicked {count} times</h1>
+      <button onClick={handleIncrease}>Click me</button>
     </div>
   );
 }
